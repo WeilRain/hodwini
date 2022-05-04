1b1f20382bc8670cd8c159f4bd72311a9ddac5c7
Weil_Rain <52824452+WeilRain@users.noreply.github.com>
2022-05-04 22:29:13 +0300

    Initial commit

---
+++ assets/js/img.js
@@ -0,0 +1,47 @@
+ // Get the modal
+ var modal = document.getElementById('id01');
+    
+ // When the user clicks anywhere outside of the modal, close it
+ window.onclick = function(event) {
+   if (event.target == modal) {
+     modal.style.display = "none";
+   }
+ }
+
+
+     // Get the modal
+    var modal = document.getElementById('id02');
+        
+    // When the user clicks anywhere outside of the modal, close it
+     window.onclick = function(event) {
+       if (event.target == modal) {
+         modal.style.display = "none";
+       }
+     }
+        // Get the modal
+    var modal = document.getElementById('id03');
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+    // Get the modal
+    var modal = document.getElementById('id04');
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+    // Get the modal
+    var modal = document.getElementById('id05');
+      
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
