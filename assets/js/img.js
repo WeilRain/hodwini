510c3459fc019e34d4d773331fd981525834f944
Weil_Rain <52824452+WeilRain@users.noreply.github.com>
2022-05-13 10:34:54 +0300

    Upgrades, People, Upgrades

--- assets/js/img.js
+++ assets/js/img.js
@@ -1,7 +1,7 @@
- // Get the modal
+
  var modal = document.getElementById('id01');
     
- // When the user clicks anywhere outside of the modal, close it
+
  window.onclick = function(event) {
    if (event.target == modal) {
      modal.style.display = "none";
@@ -9,37 +9,37 @@
  }
 
 
-     // Get the modal
+    
     var modal = document.getElementById('id02');
         
-    // When the user clicks anywhere outside of the modal, close it
+  
      window.onclick = function(event) {
        if (event.target == modal) {
          modal.style.display = "none";
        }
      }
-        // Get the modal
+       
     var modal = document.getElementById('id03');
     
-    // When the user clicks anywhere outside of the modal, close it
+  
     window.onclick = function(event) {
       if (event.target == modal) {
         modal.style.display = "none";
       }
     }
-    // Get the modal
+   
     var modal = document.getElementById('id04');
     
-    // When the user clicks anywhere outside of the modal, close it
+  
     window.onclick = function(event) {
       if (event.target == modal) {
         modal.style.display = "none";
       }
     }
-    // Get the modal
+   
     var modal = document.getElementById('id05');
       
-    // When the user clicks anywhere outside of the modal, close it
+  
     window.onclick = function(event) {
       if (event.target == modal) {
         modal.style.display = "none";
